33a8c303b5006a4d246ab04ba2becb0f7ef84f8a
Mazharul <mazharul@Mazharuls-MacBook-Pro.local>
2021-01-22 01:29:21 +0600

    Made compoment for header, navigation and footer with the views for menus

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import About from './Views/About/About';
+import Contact from './Views/Contact/Contact';
+import Home from './Views/Home/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+	return (
+		<div className='App text-gray-600 font-body'>
+			<Router>
+				<Header />
+				<div className='p-3'>
+					<Switch>
+						<Route exact path='/'>
+							<Home />
+						</Route>
+						<Route path='/about'>
+							<About />
+						</Route>
+						<Route path='/contact'>
+							<Contact />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
